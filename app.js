b90e3c3c6a229194a74902bb39b67d8c56302432
maria tektova <tkmarie@yandex.ru>
2023-05-25 15:28:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const router = require('./routes');
 const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3003 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
